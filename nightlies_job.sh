b23bbb7b5a7ad18a7e16307ea802ab1edbf3e3e6
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-02-23 06:02:24 +0000

    Test commit

--- nightlies_job.sh
+++ nightlies_job.sh
@@ -11,55 +11,55 @@ github_get() {
   echo "$body"
 }
 
-declare -a plugins_to_update=()
-update_needed=1
-for plugin in plugins/*/
-do
-    plugin=${plugin%*/}
-    plugin=${plugin##*/}
-    echo "Checking update status for $plugin"
+# declare -a plugins_to_update=()
+# update_needed=1
+# for plugin in plugins/*/
+# do
+#     plugin=${plugin%*/}
+#     plugin=${plugin##*/}
+#     echo "Checking update status for $plugin"
 
-    metadata="plugins/$plugin/metadata.json"
-    repo=$(jq -r '.repo' "$metadata")
-    hash=$(jq -r '.hash' "$metadata")
+#     metadata="plugins/$plugin/metadata.json"
+#     repo=$(jq -r '.repo' "$metadata")
+#     hash=$(jq -r '.hash' "$metadata")
 
-    IFS='/' # set delimiter
-    read -ra vals <<< "$repo" # split repo url on '/'
-    repo_owner=${vals[-2]}
-    repo_name=${vals[-1]}
-    IFS=' ' # reset delimiter
+#     IFS='/' # set delimiter
+#     read -ra vals <<< "$repo" # split repo url on '/'
+#     repo_owner=${vals[-2]}
+#     repo_name=${vals[-1]}
+#     IFS=' ' # reset delimiter
 
-    repo_info=$(github_get "repos/$repo_owner/$repo_name")
-    default_branch=$(echo "$repo_info" | jq -r '.default_branch')
+#     repo_info=$(github_get "repos/$repo_owner/$repo_name")
+#     default_branch=$(echo "$repo_info" | jq -r '.default_branch')
 
-    last_commit=$(github_get "repos/jatinchowdhury18/KlonCentaur/commits/$default_branch")
-    last_commit_hash=$(echo "$last_commit" | jq -r '.sha')
-    echo "Latest commit: $last_commit_hash"
+#     last_commit=$(github_get "repos/jatinchowdhury18/KlonCentaur/commits/$default_branch")
+#     last_commit_hash=$(echo "$last_commit" | jq -r '.sha')
+#     echo "Latest commit: $last_commit_hash"
 
-    if [[ "$hash" == "$last_commit_hash" ]]; then
-        echo "$plugin is up to date!"
-        continue
-    fi
+#     if [[ "$hash" == "$last_commit_hash" ]]; then
+#         echo "$plugin is up to date!"
+#         continue
+#     fi
 
-    echo "$plugin requires nightly update!"
-    sed -i "s/$hash/$last_commit_hash/g" "$metadata"
-    plugins_to_update+=($plugin)
-    update_needed=0
-done
+#     echo "$plugin requires nightly update!"
+#     sed -i "s/$hash/$last_commit_hash/g" "$metadata"
+#     plugins_to_update+=($plugin)
+#     update_needed=0
+# done
 
-if [[ "$update_needed" == "1" ]]; then
-    echo "All plugins up to date!"
-    exit 0
-fi
+# if [[ "$update_needed" == "1" ]]; then
+#     echo "All plugins up to date!"
+#     exit 0
+# fi
 
-git_commit_msg="Update Nightlies:"
-for plugin in $plugins_to_update
-do
-    git_commit_msg="$git_commit_msg $plugin"
-done
+# git_commit_msg="Update Nightlies:"
+# for plugin in $plugins_to_update
+# do
+#     git_commit_msg="$git_commit_msg $plugin"
+# done
 
 echo "Pushing git commit to trigger update..."
-git commit -am "$git_commit_msg"
+git commit -am "Test commit" # "$git_commit_msg"
 git push origin main
 
 echo "FINISHED"

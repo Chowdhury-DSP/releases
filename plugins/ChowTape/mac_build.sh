238b80f0f35cf70b9744177a0cfdf9902034854e
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2021-11-11 23:28:41 +0000

    ChowTape try different sshpass strategy (Update Nightlies)

--- plugins/ChowTape/mac_build.sh
+++ plugins/ChowTape/mac_build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sshpass -p '$CCRMA_PASS' scp -r jatin@ccrma-gate.stanford.edu:~/aax_builds/Mac/${plugin}.aaxplugin "${plugin}.aaxplugin"
+
 DIR=$(dirname "${BASH_SOURCE[0]}")
 SDK_PATH="$(pwd)/SDKs"
 
@@ -38,7 +40,7 @@ cp -R "build/${plugin}_artefacts/Release/Standalone/${plugin}.app" "bin/Mac/${pl
 cp -R "build/${plugin}_artefacts/Release/VST/${plugin}.vst" "bin/Mac/${plugin}.vst"
 cp -R "build/${plugin}_artefacts/Release/VST3/${plugin}.vst3" "bin/Mac/${plugin}.vst3"
 cp -R "build/${plugin}_artefacts/Release/AU/${plugin}.component" "bin/Mac/${plugin}.component"
-sshpass -p '$CCRMA_PASS' scp -r jatin@ccrma-gate.stanford.edu:~/aax_builds/Mac/${plugin}.aaxplugin "bin/Mac/${plugin}.aaxplugin"
+# sshpass -p '$CCRMA_PASS' scp -r jatin@ccrma-gate.stanford.edu:~/aax_builds/Mac/${plugin}.aaxplugin "bin/Mac/${plugin}.aaxplugin"
 
 # create installer
 echo "Creating installer..."
--- plugins/ChowTape/win_build.sh
+++ plugins/ChowTape/win_build.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+pscp -pw "$CCRMA_PASS" -r jatin@ccrma-gate.stanford.edu:~/aax_builds/Win64/${plugin}.aaxplugin "${plugin}.aaxplugin"
+
 DIR=$(dirname "${BASH_SOURCE[0]}")
 SDK_PATH="$(pwd)/SDKs"
 VST_SDK="D:${SDK_PATH:2}/VST2_SDK"
@@ -38,7 +40,7 @@ plugin=CHOWTapeModel
 cp -R "build/${plugin}_artefacts/Release/Standalone/${plugin}.exe" "bin/Win64/${plugin}.exe"
 cp -R "build/${plugin}_artefacts/Release/VST/${plugin}.dll" "bin/Win64/${plugin}.dll"
 cp -R "build/${plugin}_artefacts/Release/VST3/${plugin}.vst3" "bin/Win64/${plugin}.vst3"
-sshpass -p '$CCRMA_PASS' scp -r jatin@ccrma-gate.stanford.edu:~/aax_builds/Win64/${plugin}.aaxplugin "bin/Win64/${plugin}.aaxplugin"
+# sshpass -p '$CCRMA_PASS' scp -r jatin@ccrma-gate.stanford.edu:~/aax_builds/Win64/${plugin}.aaxplugin "bin/Win64/${plugin}.aaxplugin"
 
 cp -R "build32/${plugin}_artefacts/Release/Standalone/${plugin}.exe" "bin/Win32/${plugin}.exe"
 cp -R "build32/${plugin}_artefacts/Release/VST/${plugin}.dll" "bin/Win32/${plugin}.dll"

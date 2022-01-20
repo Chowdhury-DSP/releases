9318e24adbe7bd93c621b139ea9c9acbd729b41d
Jatin Chowdhury <jatinchowdhury18@gmail.com>
2022-01-20 19:44:44 +0000

    Make sure scritps are run in the right directory

--- plugins/BYOD/linux_build.sh
+++ plugins/BYOD/linux_build.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+# cd to the directory of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 git submodule update --init --recursive -- BYOD
--- plugins/BYOD/mac_build.sh
+++ plugins/BYOD/mac_build.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+# cd to the directory of this script
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
 git submodule update --init --recursive -- BYOD
 bash BYOD/scripts/mac_builds.sh
